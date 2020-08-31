3514a7f34e66d343d02b9217ec6fa3b58334f123
Zhaobo Wang <65083452+Zhaobo-Wang@users.noreply.github.com>
2020-08-31 05:54:30 -0400

    Add files via upload

---
+++ JAVASE/L14. 匿名内部类/Test01.java
@@ -0,0 +1,21 @@
+package demo4;
+
+public class Test01 {
+
+    static class Inner1{//静态内部类
+
+    }
+
+    class Inner2{
+
+    }
+
+    public void doSome(){
+
+        class Inner3{
+
+        }
+
+    }
+
+}
